683adce7604cb84170b7eddcb5179eeb6bcda0bd
cedric <cedric.duffournet@gmail.com>
2014-12-27 13:50:23 +0100

    remove provider

--- src/cd-crud.service.js
+++ src/cd-crud.service.js
@@ -5,135 +5,135 @@
   angular.module('cd.crud')
     .factory('cdCrud', cdCrud);
 
-  cdCrud.$inject = ['$resource', '$q', 'cdCrudProvider'];
+  cdCrud.$inject = ['$resource', '$q'];
 
   /* @ngInject */
-  function cdCrud($resource, $q, cdCrudProvider) {
+  function cdCrud($resource, $q) {
+    function cdCrudFactory(routes) {
+      var id = 0;
 
-    var id = 0;
+      var apiOptions = {
+        'findAll': {
+          method: 'GET',
+          params: {},
+          url: routes.urlFindAll
+        },
+        'find': {
+          method: 'GET',
+          params: {id: '@id'},
+          url: routes.urlFind
+        },
+        'put': {
+          method: 'PUT',
+          params: {id: '@id'},
+          url: routes.urlPut
+        },
+        'deleteItem': {
+          method: 'DELETE',
+          params: {id: '@id'},
+          url: routes.urlDeleteItem
+        }
+      };
 
-    var apiOptions = {
-      'findAll': {
-        method: 'GET',
-        params: {},
-        url: cdCrudProvider.urlFindAll
-      },
-      'find': {
-        method: 'GET',
-        params: {id: '@id'},
-        url: cdCrudProvider.urlFind
-      },
-      'put': {
-        method: 'PUT',
-        params: {id: '@id'},
-        url: cdCrudProvider.urlPut
-      },
-      'deleteItem': {
-        method: 'DELETE',
-        params: {id: '@id'},
-        url: cdCrudProvider.urlDeleteItem
+
+      var api = $resource(routes.urlPost,
+        {id: '@id'},
+        apiOptions);
+
+      var emitOnDelete = 'delete';
+
+      var service = {
+        findAll: findAll,
+        find: find,
+        put: put,
+        post: post,
+        emitOnDelete: emitOnDelete,
+        deleteItem: deleteItem
+      };
+
+      function findAll() {
+
+        var deferred = $q.defer();
+        var data = api.findAll();
+        data.$promise
+          .then(function (response) {
+            deferred.resolve(response);
+          },
+          function (error) {
+            deferred.reject(error);
+          });
+        return deferred.promise;
       }
-    };
 
 
-    var api = $resource(cdCrudProvider.urlPost,
-      {id: '@id'},
-      apiOptions);
-
-    var emitOnDelete = 'delete';
-
-    var service = {
-      findAll: findAll,
-      find: find,
-      put: put,
-      post: post,
-      emitOnDelete: emitOnDelete,
-      deleteItem: deleteItem
-    };
-
-    return service;
+      function find(pId) {
+        id = pId;
+        var deferred = $q.defer();
+        var data = api.find({id: id});
+        data.$promise
+          .then(function (response) {
+            deferred.resolve(response);
+          },
+          function (error) {
+            deferred.reject(error);
+          });
+        return deferred.promise;
+      }
 
 
-    function findAll() {
+      function put(id, dataPut) {
 
-      var deferred = $q.defer();
-      var data = api.findAll();
-      data.$promise
-        .then(function (response) {
-          deferred.resolve(response);
-        },
-        function (error) {
-          deferred.reject(error);
-        });
-      return deferred.promise;
+        var deferred = $q.defer();
+
+        var data = api.put({id: id}, dataPut);
+
+        data.$promise
+          .then(function (response) {
+            deferred.resolve(response);
+          },
+          function (error) {
+            deferred.reject(error);
+          });
+        return deferred.promise;
+      }
+
+
+      function post(dataPost) {
+
+        var deferred = $q.defer();
+
+        var data = api.save(dataPost);
+
+        data.$promise
+          .then(function (response) {
+            deferred.resolve(response);
+          },
+          function (error) {
+            deferred.reject(error);
+          });
+        return deferred.promise;
+      }
+
+      function deleteItem(id) {
+        var deferred = $q.defer();
+
+        var data = api.deleteItem({id: id});
+
+        data.$promise
+          .then(function (response) {
+            deferred.resolve(response);
+          },
+          function (error) {
+            deferred.reject(error);
+          });
+        return deferred.promise;
+      }
+
+      return service;
+
     }
 
-
-    function find(pId) {
-      id = pId;
-      var deferred = $q.defer();
-      var data = api.find({id: id});
-      data.$promise
-        .then(function (response) {
-          deferred.resolve(response);
-        },
-        function (error) {
-          deferred.reject(error);
-        });
-      return deferred.promise;
-    }
-
-
-
-    function put(id, dataPut) {
-
-
-      var deferred = $q.defer();
-
-      var data = api.put({id: id}, dataPut);
-
-      data.$promise
-        .then(function (response) {
-          deferred.resolve(response);
-        },
-        function (error) {
-          deferred.reject(error);
-        });
-      return deferred.promise;
-    }
-
-
-    function post(dataPost) {
-
-      var deferred = $q.defer();
-
-      var data = api.save(dataPost);
-
-      data.$promise
-        .then(function (response) {
-          deferred.resolve(response);
-        },
-        function (error) {
-          deferred.reject(error);
-        });
-      return deferred.promise;
-    }
-
-
-    function deleteItem(id) {
-      var deferred = $q.defer();
-
-      var data = api.deleteItem({id: id});
-
-      data.$promise
-        .then(function (response) {
-          deferred.resolve(response);
-        },
-        function (error) {
-          deferred.reject(error);
-        });
-      return deferred.promise;
-    }
+    return cdCrudFactory;
   }
 
 
